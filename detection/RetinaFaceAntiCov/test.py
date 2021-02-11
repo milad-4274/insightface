f1fd79091934551482e63f56b13bf483d52e1bce
milad yousefi <milad.4274@gmail.com>
2021-02-11 18:15:16 +0330

    change winner

--- detection/RetinaFaceAntiCov/test.py
+++ detection/RetinaFaceAntiCov/test.py
@@ -17,14 +17,17 @@ gpuid = 0
 #detector = RetinaFaceCoV('./model/mnet_cov1', 0, gpuid, 'net3')
 detector = RetinaFaceCoV('./model/mnet_cov2', 0, gpuid, 'net3l')
 
-os.mkdir("res")
+# os.mkdir("res3")
 
-images = glob.glob("images/*.jpg")
+images = glob.glob("second/images/*.jpg")
+
+# specials = [81]
 
 
-for image in images:
+for ind, image in enumerate(images):
+
     img = cv2.imread(image)
-    print(img.shape)
+    # print(img.shape)
     im_shape = img.shape
     target_size = scales[0]
     max_size = scales[1]
@@ -37,39 +40,54 @@ for image in images:
     if np.round(im_scale * im_size_max) > max_size:
         im_scale = float(max_size) / float(im_size_max)
 
-    print('im_scale', im_scale)
+    # print('im_scale', im_scale)
 
-    scales = [im_scale]
-    flip = False
+    # scales = [im_scale]
+    flip = True
 
     for c in range(count):
         faces, landmarks = detector.detect(img,
-                                        thresh,
-                                        scales=scales,
-                                        do_flip=flip)
+                                           thresh,
+                                           scales=[im_scale],
+                                           do_flip=flip)
 
-    if faces is not None:
-        print('find', faces.shape[0], 'faces')
-        for i in range(faces.shape[0]):
-            #print('score', faces[i][4])
-            face = faces[i]
-            box = face[0:4].astype(np.int)
-            mask = face[5]
-            print(i, box, mask)
-            #color = (255,0,0)
-            if mask >= mask_thresh:
-                color = (0, 0, 255)
-            else:
-                color = (0, 255, 0)
-            cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
-            landmark5 = landmarks[i].astype(np.int)
-            #print(landmark.shape)
-            for l in range(landmark5.shape[0]):
-                color = (255, 0, 0)
-                cv2.circle(img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+    if faces is not None and len(faces) != 0:
+        # print("name", image)
+        # print('find', faces.shape[0], 'faces',faces)
+        winner = 0
+        largest = 0
+        w = np.argmax(faces[:, 4])
+        for i,face in enumerate(faces):
+            if (face[2] - face[0])+(face[3]-face[1]) > largest:
+                largest = face[2] - face[0]
+                winner = i
+        face = faces[winner]
+        # for i in range(faces.shape[0]):
+        # print('score', faces[i][4])
 
-        filename = 'res/' + image + '.jpg'
-        print('writing', filename)
-        cv2.imwrite(filename, img)
+        # face = faces[i]
+        # print(len(face), face, "faceeee")
+        box = face[0:4].astype(np.int)
+        mask = face[5]
+        # print(i, box, mask)
+        #color = (255,0,0)
+        if mask >= mask_thresh:
+            color = (0, 0, 255)
+        else:
+            color = (0, 255, 0)
+        cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), color, 2)
+        # cv2.putText(img, str(face[4]*100), (box[0], box[1]),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2)
+        landmark5 = landmarks[w].astype(np.int)
+        #print(landmark.shape)
+        for l in range(landmark5.shape[0]):
+            color = (255, 0, 0)
+            cv2.circle(
+                img, (landmark5[l][0], landmark5[l][1]), 1, color, 2)
+
+        # filename = 'res3/' + str(ind) + '.jpg'
+        # if ind in specials:
+        # print('writing', filename)
+        # cv2.imwrite(filename, img)
     else:
-        print("faces is none",image)
+        print("faces is none", image)
